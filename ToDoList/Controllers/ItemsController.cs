5a3d97a6dae524f9de2bd140f7f44c748b1d879e
Derrak Richard <derrak@gmail.com>
2022-07-20 10:02:54 -0700

    add code to find item with unique ID and delete lists
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -7,24 +7,38 @@ namespace ToDoList.Controllers
   public class ItemsController : Controller
   {
 
-    [HttpGet("/items")] 
+    [HttpGet("/items")]
     public ActionResult Index()
     {
       List<Item> allItems = Item.GetAll();
       return View(allItems);
     }
-    
+
     [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
 
-      [HttpPost("/items")]
+    [HttpPost("/items")]
     public ActionResult Create(string description)
     {
       Item myItem = new Item(description);
       return RedirectToAction("Index");
+    }
+
+    [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
+    }
+
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+      Item.ClearAll();
+      return View();
     }
 
   }
